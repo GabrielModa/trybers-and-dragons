75ca6c5d5e1b1f0b3d131e7fc5f1379435286e9f
Gabriel Moda <gabriel-moda@hotmail.com.br>
2022-04-25 15:58:54 -0300

    [01 OK]

--- tests/sources/Races.createdRacesInstances.error.ts
+++ tests/sources/Races.createdRacesInstances.error.ts
@@ -3,7 +3,11 @@ import Race from '../../src/Races';
 let ok = true;
 try {
   Race.createdRacesInstances();
+  console.log('try');
+  
 } catch (err: any) {
+  console.log('catch');
+
   if (err.message === 'Not implemented') {
     ok = false;
   }
