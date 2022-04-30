c2be219de6d6e0b1203c3c3a2f2e37765f476f9f
Gabriel Moda <gabriel-moda@hotmail.com.br>
2022-04-30 17:03:20 -0300

    [07 fixing, import], [08 OK]

--- src/Fighter/index.ts
+++ src/Fighter/index.ts
@@ -1,3 +1,8 @@
 import Figther from './Figther';
+import SimpleFighter from './SimpleFighter';
+
+export {
+  SimpleFighter,
+};
 
 export default Figther;
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import Character from './Character';
-import * as teste from './Races';
-
-const tes = new Character('2');
-let tetete = tes.defense;
-tetete = 4;
-console.log(tetete);
-console.log(tes.defense);
